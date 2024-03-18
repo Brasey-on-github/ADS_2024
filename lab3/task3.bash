aa81399b7efe9984cfa6672d6c1d9e6adf1b9603
Loic Brasey <loibrasey@heig-vd.ch>
2024-03-18 16:41:10 +0100

    caca

--- lab3/task3.bash
+++ lab3/task3.bash
@@ -1,2 +1,1 @@
-echo "Number, Date " > access.csv && \
-cut -d$'\t' -f3 < ads_website.log | egrep '[[:digit:]]{2,2}/[[:alpha:]]{3,3}/[[:digit:]]{4,4}' -o | uniq -c | sed -e 's/  *//' | sed -e 's/ /, /' | sed -e 's/\//./'>> access.csv
+echo "Number, Date " > access.csv && cut -d$'\t' -f3 < ads_website.log | egrep '[[:digit:]]{2,2}/[[:alpha:]]{3,3}/[[:digit:]]{4,4}' -o | uniq -c | sed -e 's/  *//' | sed -e 's/ /,'\''/' | sed -e 's/\//. /g'>> access.csv
